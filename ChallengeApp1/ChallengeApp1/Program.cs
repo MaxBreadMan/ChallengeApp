19c8e41e8810d3e950db282c026cd43c27055cff
Przemyslaw Wolski <wolski802@gmail.com>
2024-01-17 16:20:08 +0000

    ConsoleApp1

--- ChallengeApp1/ChallengeApp1/Program.cs
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// ==
+// !=
+// >
+// <
+// <=
+// >=
+
+
+// && i 
+// || lub
+// ! negacja
+
+
+var name = "Przemek";
+var age = 33;
+
+
+if (age > 50)
+{
+    if (name == "Przemek")
+    {
+        Console.WriteLine("Jestem Przemek lub mam mniej niz 50 lat ");
+    }
+    
+
+}
+else
+{
+
+
+
+    Console.WriteLine(" 40 - 50 latek");
+}
