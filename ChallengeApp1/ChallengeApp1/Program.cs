bb05b452fc75c51d95c06f6a8dcce9016e1a6f88
Przemyslaw Wolski <wolski802@gmail.com>
2024-02-04 03:51:58 +0000

    przygotowanie pod 12

--- ChallengeApp1/ChallengeApp1/Program.cs
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -11,7 +11,7 @@ employee.AddGrade(2);
 employee.AddGrade(6);
 
 var statistics = employee.GetStatistics();
-Console.WriteLine("-------GetStatistics-------");
+Console.WriteLine("-------> Statystyki <-------");
 Console.WriteLine("Imie: " + employee.Name);
 Console.WriteLine("Nazwisko: " + employee.Surname);
 Console.WriteLine("Staz: " + employee.Seniority);
@@ -19,43 +19,3 @@ Console.WriteLine("Oceny pracownika: ");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
 Console.WriteLine($"Average: {statistics.Average:N2}");
-
-var statistics1 = employee.GetStatisticsForEach();
-Console.WriteLine("--------GetStatisticsForEach--------");
-Console.WriteLine("Imie: " + employee.Name);
-Console.WriteLine("Nazwisko: " + employee.Surname);
-Console.WriteLine("Staz: " + employee.Seniority);
-Console.WriteLine("Oceny pracownika: ");
-Console.WriteLine($"Min: {statistics1.Min}");
-Console.WriteLine($"Max: {statistics1.Max}");
-Console.WriteLine($"Average: {statistics1.Average:N2}");
-
-var statistics2 = employee.GetStatisticsWithDoWhile();
-Console.WriteLine("--------GetStatisticsWithDoWhile--------");
-Console.WriteLine("Imie: " + employee.Name);
-Console.WriteLine("Nazwisko: " + employee.Surname);
-Console.WriteLine("Staz: " + employee.Seniority);
-Console.WriteLine("Oceny pracownika: ");
-Console.WriteLine($"Min: {statistics2.Min}");
-Console.WriteLine($"Max: {statistics2.Max}");
-Console.WriteLine($"Average: {statistics2.Average:N2}");
-
-var statistics3 = employee.GetStatisticsWithWhile();
-Console.WriteLine("--------GetStatisticsWithWhile--------");
-Console.WriteLine("Imie: " + employee.Name);
-Console.WriteLine("Nazwisko: " + employee.Surname);
-Console.WriteLine("Staz: " + employee.Seniority);
-Console.WriteLine("Oceny pracownika: ");
-Console.WriteLine($"Min: {statistics3.Min}");
-Console.WriteLine($"Max: {statistics3.Max}");
-Console.WriteLine($"Average: {statistics3.Average:N2}");
-
-var statistics4 = employee.GetStatisticsWithFor();
-Console.WriteLine("--------GetStatisticsWithFor--------");
-Console.WriteLine("Imie: " + employee.Name);
-Console.WriteLine("Nazwisko: " + employee.Surname);
-Console.WriteLine("Staz: " + employee.Seniority);
-Console.WriteLine("Oceny pracownika: ");
-Console.WriteLine($"Min: {statistics4.Min}");
-Console.WriteLine($"Max: {statistics4.Max}");
-Console.WriteLine($"Average: {statistics4.Average:N2}");
